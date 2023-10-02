4d06aa9e68d24e0a225accc19414b62c32ecc95f
rdinnager <r.dinnage@gmail.com>
2023-10-02 09:32:07 -0400

    switched to a U-net like architecture for the classification and phylogeny-conditioner networks. also upped image resolution to 320 x 320 instead of 224 x 224 after reading that this can increase accuracy slightly, seems to have worked.

--- R/test_images.R
+++ R/test_images.R
@@ -1,13 +1,37 @@
 library(torch)
 library(torchvision)
+library(imager)
 
 image_files <- list.files("data/inat/images", full.names = TRUE)
 
+res <- list()
+j <- 0
+
+image_files <- image_files[which(image_files == "data/inat/images/2759396_medium.PNG"):length(image_files)]
+
 for(i in image_files) {
-  im <- base_loader(i)
-  if(dim(im)[3] != 3) {
+  im <- try(base_loader(i))
+  if(inherits(im, "try-error")) {
+    print("error!")
     print(i)
+    next
   }
+  if(dim(im)[3] != 3) {
+    im <- load.image(i)
+    im <- flatten.alpha(im)
+    ext <- fs::path_ext(i)
+    if(toupper(ext) == ext) {
+      new_i <- fs::path_ext_set(i, tolower(ext))
+      save.image(im, new_i)
+      fs::file_move(new_i, i)
+    } else {
+      save.image(im, i)
+    }
+    message("resaved image ", i)
+  }
+  j <- j + 1
+  res[[j]] <- dim(im)
+  print(j)
 }
 
 ## looks like base_loader doesn't deal with pngs well
